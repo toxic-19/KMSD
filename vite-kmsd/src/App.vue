60683d6c28e50a008c478817a8de9d565e3e45d5
toxic <3468528598@qq.com>
2024-02-05 18:35:11 +0800

    feat: add stylelint

--- vite-kmsd/src/App.vue
+++ vite-kmsd/src/App.vue
@@ -1,11 +1,13 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div class="app-container">我是app组件</div>
+  <div class="app-container">
+    <h1>我是app组件</h1>
+  </div>
 </template>
 
 <style scoped lang="scss">
-
+h1 {
+  font-size: 16px;
+}
 </style>
